b0b18b37652f67a41b4ca5dbdc0f5431edee67cf
Adam Nowosielecki <nowosieleckilis@gmail.com>
2022-10-25 11:34:59 +0200

    smallfix

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const PORT = 8080;
 
 app.get("/kotek", (req, res) => {
   res.status(200).send({
-    race: 'cat',
-    name: 'Blue',
-    size: 'big',
-    furcolor: 'blue',
-    eyes: 'green'
-  })
+    race: "cat",
+    name: "Blue",
+    size: "big",
+    furcolor: "blue",
+    eyes: "green",
+  });
 });
+
+app.listen(PORT, () => console.log(`it's working on http://localhost:${PORT}`));
